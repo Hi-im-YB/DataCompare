08c1aa09e694911bac5f3a345745bd02dc9d3806
NuclearRazor <nuclearrazor@gmail.com>
2019-10-04 17:13:32 +0300

    add age group to company info

--- src/source.py
+++ src/source.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+#Company Specifier -> Company YrsExperience c [0-2, 3-5, 6-10, 11-15, 16+]
+#Company -> Company YrsCompany
+#Company YrsExperience = min -> max Company YrsExperience : YrsCompany = [0-2] && [3-5] && [6-10] && [11-15] && [16+]
+#YrsCompany, Company are the same length - data with equal row numbers for each column
+
 #group age slices
 FIRST_GROUP = [0.0, 1.0, 2.0]
 SECOND_GROUP = [3.0, 4.0, 5.0]
@@ -17,10 +22,6 @@ YEARS_SLICE = {'FIRST_GROUP' : FIRST_GROUP,
 #exist data file
 INPUT_DATA_PATH = '../common/data.csv'
 input_file = pd.read_csv(INPUT_DATA_PATH)
-
-#Company Specifier -> Company YrsExperience c [0-2, 3-5, 6-10, 11-15, 16+]
-#Company -> Company YrsCompany
-#Company YrsExperience = min -> max Company YrsExperience : YrsCompany = [0-2] && [3-5] && [6-10] && [11-15] && [16+]
 
 def is_year_in_years_slice(min_company_age):
     #by default return 1 st group if None age bound/...
@@ -53,40 +54,41 @@ company_profile['Company'] = company_name
 company_profile['YrsCompany'] = company_years
 company_profile['YrsExperience'] = company_experience
 
-#print(pdf(input_file).loc[pdf(input_file)['YrsCompany'] == company_profile['YrsExperience']][0:10])
-
-#YrsCompany, Company are the same length - data with equal row numbers for each column
-print("Unfiltered companies & ages length: ", len(company_profile['YrsExperience']))
-
 #filter by unique rows
+print("Unfiltered companies & ages length: ", len(company_profile['YrsExperience']))
 company_profile_filtered = list()
 for company_info in zip(company_profile['Company'], company_profile['YrsExperience']):
     company_profile_filtered.append(company_info)
 company_profile_filtered = list(set(company_profile_filtered))
 
-print("Filtered companies & ages length: ", len(company_profile_filtered))
-
-company_ages_by_names_raw = {}
 #make list of tuples with [(Company, Age1, Age2, ..., AgeN)]
+print("Filtered companies & ages length: ", len(company_profile_filtered))
+company_ages_by_names_raw = {}
 for c_name, c_age in company_profile_filtered:
     company_ages_by_names_raw.setdefault(c_name, [c_name]).append(c_age)
 company_ages_by_names = list(map(tuple, company_ages_by_names_raw.values()))
 
-print("Filtered companies by ages length: ", len(company_ages_by_names))
-
 #find minimum company age and search if that value in group ages slice
+print("Filtered companies by ages length: ", len(company_ages_by_names))
+company_data_result = list()
 for company_info in company_ages_by_names:
-    print('company name', company_info[0], 'ages: ', company_info[1:-1])
-    min_company_age = 0.0
+    #print('company name', company_info[0], 'ages: ', company_info[1:-1])
 
+    min_company_age = 0.0
     try:
         min_company_age = min(company_info[1:-1])
     except ValueError:
-        print('Company have not age bounds, suppose that necessary experience is 0 years')
+        #print('Company have not age bounds, suppose that necessary experience is 0 years')
+        pass
 
-    print('minimum age: ', min_company_age)
-    print('ages slice: ', is_year_in_years_slice(min_company_age))
+    company_data_result.append((company_info[0], company_info[1:-1], is_year_in_years_slice(min_company_age)))
 
+    #print('minimum age: ', min_company_age)
+    #print('ages slice: ', is_year_in_years_slice(min_company_age))
 
-#for each company value find minimum, if minimum c YEARS_SLICE
-#save this company with key of YEARS_SLICE dict
+for company_info in company_data_result:
+    print(company_info)
+
+#TODO
+#- if company in all groups (shift minimum age)
+#- for each Company find product value, multiplied by, we have all age groups etries
